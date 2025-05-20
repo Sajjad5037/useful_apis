83ea6b53849b514e8c0585bda9ba3db4e8f99eec
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-20 14:15:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ Base = declarative_base()
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
-BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 
 
