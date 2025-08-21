afd370459d494a7da2412e147aea7c9f681963bc
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-08-21 14:21:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1462,16 +1462,18 @@ async def evaluate_student_response_from_images(
 
         # --- Step 2: Retrieve instructions with dynamic k ---
         print("[DEBUG] Retrieving relevant instructions from vector store...")
-        #retrieval_query = (
-         #  f"{question_text}"
-        #)
+       retrieval_query = (
+           f"Provide all instructions, features, and marking rules relevant for answering: "
+           f"{question_text}"
+        )
+
 
         
-        retrieval_query = f"""
-        Instructions for evaluating student responses: 
-        Include all marking rules, features, and point allocation guidelines. 
-        Question: {question_text}
-        """
+ #       retrieval_query = f"""
+  #      Instructions for evaluating student responses: 
+   #     Include all marking rules, features, and point allocation guidelines. 
+    #    Question: {question_text}
+     #   """
 
 
         
@@ -4055,6 +4057,7 @@ async def chat_quran(msg: Message):
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
     
+
 
 
 
