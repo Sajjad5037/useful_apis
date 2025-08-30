8166a83f3d9bee7ab291f7d36d436a14366b126b
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-08-30 16:27:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import docx2txt
 import os
 import vertexai
@@ -5090,6 +5091,7 @@ async def chat_quran(msg: Message):
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
     
+
 
 
 
